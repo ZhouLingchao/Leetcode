67542a54ee76816e586d681162bbf62ccc18841a
zhtyrion <522572246@qq.com>
2019-05-20 20:37:28 +0800

    two solutions

---
+++ medium/BinarySearchTreeToGreaterSumTree.cs
@@ -0,0 +1,32 @@
+
+using System.Collections.Generic;
+using leetcode.fundament;
+
+namespace leetcode.medium
+{
+  public class BinarySearchTreeToGreaterSumTree
+  {
+    public TreeNode BstToGst(TreeNode node)
+    {
+      var sum = 0;
+      var orderedList = GetOrderedList(node);
+      foreach(var orderedNode in orderedList){
+        sum += orderedNode.val;
+        orderedNode.val = sum;
+      }
+      return node;
+    }
+
+    private IList<TreeNode> GetOrderedList(TreeNode node){
+      var list =  new List<TreeNode>();
+      BuildList(list,node);
+      return list;
+    }
+    private void BuildList(IList<TreeNode> list, TreeNode node){
+      if(node == null) return ;
+      BuildList(list,node.right);
+      list.Add(node);
+      BuildList(list,node.left);
+    }
+  }
+}
---
+++ medium/ConstructBinarySearchTreeFromPreorderTraversal.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using leetcode.fundament;
+
+namespace leetcode.medium
+{
+  public class ConstructBinarySearchTreeFromPreorderTraversal
+  {
+    public TreeNode BstFromPreorder(int[] preorder)
+    {
+      var root = new TreeNode(preorder[0]);
+      for (var i = 1; i < preorder.Length; i++)
+      {
+        Location(root, preorder[i]);
+      }
+      return root;
+    }
+    private void Location(TreeNode node, int value)
+    {
+      if(value>node.val){
+        if(node.right==null) node.right = new TreeNode(value);
+        else Location(node.right,value);
+      }else{
+        if(node.left==null) node.left = new TreeNode(value);
+        else Location(node.left,value);
+      }
+    }
+  }
+}
