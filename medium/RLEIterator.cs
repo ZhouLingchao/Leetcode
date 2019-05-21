cdb4419b67afedb694b5e0f3df8881d3521d0c9c
tyzhou <522572246@qq.com>
2019-05-21 15:14:45 +0800

    RLEterator passed

--- medium/RLEIterator.cs
+++ medium/RLEIterator.cs
@@ -4,8 +4,10 @@ namespace Leetcode.medium
   public class RLEIterator
   {
     private int[] _data;
-    private int _count;
     private int _index;
+    private int _used;
+    private long _count;
+    private long _current;
     public RLEIterator(int[] A)
     {
       _data = A;
@@ -16,9 +18,27 @@ namespace Leetcode.medium
     }
     public int Next(int n)
     {
-      _index += n;
-      if(_index > _data.Length) return -1;
-      else return _data[_index-1];
+      _current += n;
+      if (_current >= _count) return -1;
+
+      int result = 0;
+      while (n >= 0)
+      {
+        if (n + _used >= _data[_index])
+        {
+          n -= (_data[_index] - _used);
+          _used = 0;
+          _index += 2;
+          if (n == 0) return _data[_index - 1];
+        }
+        else
+        {
+          _used += n;
+          result = _data[_index + 1];
+          break;
+        }
+      }
+      return result;
     }
   }
 }
