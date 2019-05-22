68b10350ccb6bc1efcd8701d999518fca1060cca
zhtyrion <522572246@qq.com>
2019-05-22 23:41:25 +0800

    empty class of TrimABinarySearchTree

---
+++ easy/TrimABinarySearchTree.cs
@@ -0,0 +1,12 @@
+using Leetcode.fundament;
+
+namespace Leetcode.easy
+{
+  public class TrimABinarySearchTree
+  {
+    public TreeNode TrimBST(TreeNode root, int L, int R)
+    {
+        
+    }
+  }
+}
