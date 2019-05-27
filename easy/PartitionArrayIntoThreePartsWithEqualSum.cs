39d5aa7a0dde0e85e342e21d3687ac359ac4a79c
tyzhou <522572246@qq.com>
2019-05-27 11:04:29 +0800

    PartitionArrayIntoThreePartsWithEqualSum

--- easy/PartitionArrayIntoThreePartsWithEqualSum.cs
+++ easy/PartitionArrayIntoThreePartsWithEqualSum.cs
@@ -8,7 +8,20 @@ namespace Leetcode.easy
     {
       var sum = A.Sum();
       if (sum % 3 != 0) return false;
-      
+      var target = sum /3;
+
+      var s = 0;
+      var count = 0;
+      foreach (var item in A)
+      {
+          s += item;
+          if(s == target) {
+            s = 0;
+            count++;
+            if(count == 2) return true;
+          }
+      }
+      return false;
     }
   }
 }
